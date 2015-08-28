872ddb58eaaff5a60dab516bd21a5894d64ef734
matthiasadriaens <matthiasadriaens@gmail.com>
2015-08-28 19:49:32 +0200

    First commit

---
+++ resolutions.js
@@ -0,0 +1,55 @@
+Resolutions = new Mongo.Collection('resolutions');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+     resolutions: function() {
+       if (Session.get('hideFinished')){
+         return Resolutions.find({checked: {$ne: true}});
+       }
+       else{
+         return Resolutions.find();
+       }
+     },
+     hideFinished: function(){
+       return Session.get('hideFinished');
+     }
+  });
+
+  Template.body.events({
+    'submit .new-resolution': function(event){
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        Title : title,
+        createdAt : new Date ()
+      });
+
+      event.target.title.value = "";
+
+      return false;
+    },
+    'change .hide-finished': function(event) {
+      Session.set('hideFinished',event.target.checked);
+    }
+  });
+
+  Template.resolution.events({
+    'click .toggle-checked': function(){
+      Resolutions.update(this._id, {$set: {
+        checked: !this.checked
+      }})
+    },
+    'click .delete': function(){
+      Resolutions.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+  });
+}
